2b4a737e564437e44b94e19c1ef4e480e68b79d0
Laptop-HP <micha.ogorek@op.pl>
2026-01-19 19:15:29 +0100

    Init Spring Boot

---
+++ src/main/java/org/example/io_lab_11_12/IoLab1112Application.java
@@ -0,0 +1,13 @@
+package org.example.io_lab_11_12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoLab1112Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoLab1112Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/io_lab_11_12/IoLab1112ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.io_lab_11_12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoLab1112ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
